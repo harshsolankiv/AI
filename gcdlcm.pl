a4496aed8584148939bd3a07426fd06dd953afbb
harshsolankiv <89843412+harshsolankiv@users.noreply.github.com>
2022-05-09 21:13:57 +0530

    Update gcdlcm.pl

--- gcdlcm.pl
+++ gcdlcm.pl
@@ -5,10 +5,5 @@ gcd(X, Y, G) :-
   gcd(X, Y1, G).
 gcd(X, Y, G) :- X > Y, gcd(Y, X, G).
 
-gcd(X, Y, G) :- X = Y, G = X.
-gcd(X, Y, G) :-
-  X < Y,
-  Y1 is Y-X,
-  gcd(X, Y1, G).
-gcd(X, Y, G) :- X > Y, gcd(Y, X, G).
+
 lcm(X,Y,LCM):-gcd(X,Y,GCD), LCM is X*Y//GCD.
